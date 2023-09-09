d2368432dec214d130a52289289bbe3306ae8d00
gowsi tamil <ttgowsikan@gmail.com>
2023-09-09 11:58:37 +0530

    commfive

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const app = express();
-const server = require("http").Server(app);
-const io = require("socket.io")(server); // Pass the server instance to Socket.IO
+const http = require("http");
+
+const server = http.createServer(app);
+const socketIO = require("socket.io");
+
+const io = socketIO(server);
 //const Player = require("./database.js");
 const mongoose = require("mongoose");
 require("dotenv").config();
